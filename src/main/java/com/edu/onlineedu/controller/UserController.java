b78eb5237de4d4a424ddd5f4ea179e7468edb36e
15217814598 <643560027@qq.com>
2021-03-16 19:43:20 +0800

    添加登录

--- src/main/java/com/edu/onlineedu/controller/UserController.java
+++ src/main/java/com/edu/onlineedu/controller/UserController.java
@@ -3,18 +3,22 @@ package com.edu.onlineedu.controller;
 import com.edu.onlineedu.pojo.Result;
 import com.edu.onlineedu.pojo.User;
 import com.edu.onlineedu.service.UserService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Objects;
 
 @RestController
 public class UserController {
+
+    Logger logger = LoggerFactory.getLogger(this.getClass());
     @Autowired
     private UserService userService;
 
@@ -24,10 +28,11 @@ public class UserController {
                 return new Result(false, "账号或密码不能为空");
         }
         User user1 = userService.fingByName(user);
+        logger.warn(user1.toString());
         if (user1 == null) {
             return new Result(false, "账号不存在");
         }
-        if (user1.getPassword() != user.getPassword()) {
+        if (!Objects.equals(user.getPassword(), user1.getPassword())) {
             return new Result(false, "密码错误");
         }
         Map<String, Object> data = new HashMap<>();
